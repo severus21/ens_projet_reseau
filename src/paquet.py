f94c05aad71df861560fa7649ff7524a0d34ba12
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-11-20 16:32:01 +0100

    paquet

--- src/paquet.py
+++ src/paquet.py
@@ -13,10 +13,10 @@ def PadN (mbz) :
 def IHU (_id):
     n = len(_id)
     _type = (2).to_bytes(1, byteorder = 'big')
-    length = n.to_bytes(1, byteorder = 'big')
-    return _type + length + _id
+    length = (8).to_bytes(1, byteorder = 'big')
+    return _type + length + _id.to_bytes(8, byteorder='big')
      
-def Neighbourg_Request ():
+def NeighbourgRequest ():
     _type = (3).to_bytes(1, byteorder = 'big')
     length = (0).to_bytes(1, byteorder = 'big')
     return _type + length
@@ -48,15 +48,17 @@ def IHave(_id,seqno):
     return _type + length + seqno +_id 
 
 
-def make_paquet (self__id, l):
+def make_paquet (_id0, l):
     body = b''
     for tlv in l :
         body += tlv
+
+    _id = (_id0).to_bytes(8, byteorder='big')   
     magic = (57).to_bytes(1, byteorder = 'big')
     version = (0).to_bytes(1, byteorder = 'big')
     n = len(body)
     length = n.to_bytes(2, byteorder = 'big')
-    return magic + version + length + self__id + body
+    return magic + version + length + _id + body
  
    
 def to_one_tlv (paquet, i):
@@ -68,30 +70,30 @@ def to_one_tlv (paquet, i):
         return (Msg.PadN, None), i + 2 + length
     elif _type == 2:
         length = paquet[i+1]
-        _id = paquet[i+2:i+10]
+        _id = int.from_bytes(paquet[i+2:i+10], byteorder='big') 
         return (Msg.IHU, _id),i+2+length
     elif _type == 4:
         length = paquet[i+1]
         j = i+2
         l = []
         while (j < i+2+length-7):
-            _id = paquet[j:j+8]
-            ip = paquet[j+8:j+24]
-            port = paquet[j+24:j+26]
+            _id = int.from_bytes(paquet[j:j+8], byteorder='big')
+            ip = int.from_bytes(paquet[j+8:j+24], byteorder='big')
+            port = int.from_bytes(paquet[j+24:j+26], byteorder='big')
             j = j+26
             l.append((_id,(ip,port)))
         return (Msg.NR, l), i+2+length 
         
     elif _type == 5:
         length = paquet[i+1]-12
-        seqno = paquet[i+2:i+6]
-        _id = paquet[i+6:i+14]
+        seqno = int.from_bytes(paquet[i+2:i+6], byteorder='big')
+        _id = int.from_bytes(paquet[i+6:i+14], byteorder='big')
         data = paquet[i+14:i+14+length]
         return (Msg.N, (_id, seqno, data)), i+14+length
     elif _type == 6:
         length = paquet[i+1]
-        seqno = paquet[i+2:i+6]
-        _id = paquet[i+6:i+14]
+        seqno = int.from_bytes(paquet[i+2:i+6], byteorder='big')
+        _id = int.from_bytes(paquet[i+6:i+14], byteorder='big')
         return (Msg.Data, (_id, seqno)), i+2+length
     else :
         length = paquet[i+1]
@@ -101,7 +103,7 @@ def to_one_tlv (paquet, i):
 
 def to_tlvs (paquet):
     n = int.from_bytes(paquet[2:4], byteorder = 'big')
-    _id = paquet[4:12]
+    _id = int.from_bytes(paquet[4:12], byteorder = 'big')
     l = []
     i = 12
     while i < 12 + n:
